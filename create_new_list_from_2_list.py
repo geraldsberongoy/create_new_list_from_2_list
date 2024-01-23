45e71951a19ac6fb85ba56ed7a05be6da4256e24
geraldsberongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2024-01-23 19:13:34 +0800

    for loops

--- create_new_list_from_2_list.py
+++ create_new_list_from_2_list.py
@@ -20,3 +20,11 @@
 list1 = [10, 20, 25, 30, 35]
 list2 = [40, 45, 60, 75, 90]
 result_list = []
+
+# For loop
+for i in list1:
+    if i % 2 != 0:
+        result_list.append(i)
+for i in list2:
+    if i % 2 == 0:
+        result_list.append(i)
