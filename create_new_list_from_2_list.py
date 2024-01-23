4b7478595aa30523420ee602b937168b1243a4a7
geraldsberongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2024-01-23 19:11:29 +0800

    Pseudocode

--- create_new_list_from_2_list.py
+++ create_new_list_from_2_list.py
@@ -6,3 +6,10 @@
 
 # Expected Output:
 # result list: [25, 35, 40, 60, 90]
+
+# Pseudocode
+# assignning variable to two list
+# assigning empty variable for result_list
+# for loop with nested conditional to add the odd first list in result list
+# another for loop with nested conditional to add the even second list in result list
+# print result list
