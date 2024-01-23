4c5e575689cdf3e7009f3a0912a849f12999b7f2
geraldsberongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2024-01-23 18:52:51 +0800

    Given and Expected Output

--- create_new_list_from_2_list.py
+++ create_new_list_from_2_list.py
@@ -1,1 +1,8 @@
 # Exercise 10: Create a new list from two list using the following condition
+
+# Given:
+# list1 = [10, 20, 25, 30, 35]
+# list2 = [40, 45, 60, 75, 90]
+
+# Expected Output:
+# result list: [25, 35, 40, 60, 90]
